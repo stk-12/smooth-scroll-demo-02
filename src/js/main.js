9f469b2bb48310fb1c4643e700d558a5db893d74
keisuke sato <corydoras121@gmail.com>
2023-01-22 14:36:21 +0900

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,52 @@
+import '../css/style.scss';
+import Lenis from '@studio-freight/lenis'
+
+const lenis = new Lenis({
+  duration: 2.0,
+  // easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+  // direction: 'vertical', // vertical, horizontal
+  // gestureDirection: 'vertical', // vertical, horizontal, both
+  // smooth: true,
+  // mouseMultiplier: 1,
+  // smoothTouch: false,
+  // touchMultiplier: 2,
+  // infinite: false,
+})
+
+//get scroll value
+// lenis.on('scroll', ({ scroll, limit, velocity, direction, progress }) => {
+//   console.log({ scroll, limit, velocity, direction, progress })
+// })
+
+function update(time) {
+  lenis.raf(time);
+  requestAnimationFrame(update);
+}
+
+requestAnimationFrame(update);
+
+
+const anchors = document.querySelectorAll('.anchor');
+
+anchors.forEach((anchor)=>{
+  anchor.addEventListener('click', (e)=>{
+    const target = anchor.hash;
+    // lenis.scrollTo(target); 
+    lenis.scrollTo(target, {
+      duration: 2.0,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      // easing: (x) => x < 0.5 ? 4 * x * x * x : 1 - Math.pow(-2 * x + 2, 3) / 2,
+    }); 
+  });
+});
+
+const pagetop = document.querySelector('.pagetop');
+
+pagetop.addEventListener('click', (e)=>{
+  e.preventDefault();
+  lenis.scrollTo(0, {
+    duration: 2.0,
+    // easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+  }); 
+});
+
