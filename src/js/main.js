bbe35e56102499d1b1b02bfb75a4d172fdb1d7d8
keisuke sato <corydoras121@gmail.com>
2023-01-22 14:52:08 +0900

    [update]クラスに変更

--- src/js/main.js
+++ src/js/main.js
@@ -1,52 +1,61 @@
 import '../css/style.scss';
 import Lenis from '@studio-freight/lenis'
 
-const lenis = new Lenis({
-  duration: 2.0,
-  // easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-  // direction: 'vertical', // vertical, horizontal
-  // gestureDirection: 'vertical', // vertical, horizontal, both
-  // smooth: true,
-  // mouseMultiplier: 1,
-  // smoothTouch: false,
-  // touchMultiplier: 2,
-  // infinite: false,
-})
+class Main {
+  constructor() {
+    this.DOM = {};
+    this.DOM.anchors = document.querySelectorAll('.anchor');
+    this.DOM.pagetop = document.querySelector('.pagetop');
 
-//get scroll value
-// lenis.on('scroll', ({ scroll, limit, velocity, direction, progress }) => {
-//   console.log({ scroll, limit, velocity, direction, progress })
-// })
+    this.lenis = new Lenis({
+      duration: 2.0,
+      // easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      // direction: 'vertical', // vertical, horizontal
+      // gestureDirection: 'vertical', // vertical, horizontal, both
+      // smooth: true,
+      // mouseMultiplier: 1,
+      // smoothTouch: false,
+      // touchMultiplier: 2,
+      // infinite: false,
+    });
 
-function update(time) {
-  lenis.raf(time);
-  requestAnimationFrame(update);
+    this._addEvent();
+    this._init();
+  }
+
+  _update(time) {
+    this.lenis.raf(time);
+    requestAnimationFrame(this._update.bind(this));
+  }
+
+  _addEvent() {
+
+    this.DOM.anchors.forEach((anchor)=>{
+      anchor.addEventListener('click', (e)=>{
+        const target = anchor.hash;
+        // this.lenis.scrollTo(target); 
+        this.lenis.scrollTo(target, {
+          duration: 2.0,
+          easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+          // easing: (x) => x < 0.5 ? 4 * x * x * x : 1 - Math.pow(-2 * x + 2, 3) / 2,
+        }); 
+      });
+    });
+
+    this.DOM.pagetop.addEventListener('click', (e)=>{
+      e.preventDefault();
+      this.lenis.scrollTo(0, {
+        duration: 2.0,
+        // easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      }); 
+    });
+  }
+
+  _init() {
+    requestAnimationFrame(this._update.bind(this));
+  }
 }
 
-requestAnimationFrame(update);
+new Main();
 
-
-const anchors = document.querySelectorAll('.anchor');
-
-anchors.forEach((anchor)=>{
-  anchor.addEventListener('click', (e)=>{
-    const target = anchor.hash;
-    // lenis.scrollTo(target); 
-    lenis.scrollTo(target, {
-      duration: 2.0,
-      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      // easing: (x) => x < 0.5 ? 4 * x * x * x : 1 - Math.pow(-2 * x + 2, 3) / 2,
-    }); 
-  });
-});
-
-const pagetop = document.querySelector('.pagetop');
-
-pagetop.addEventListener('click', (e)=>{
-  e.preventDefault();
-  lenis.scrollTo(0, {
-    duration: 2.0,
-    // easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-  }); 
-});
 
